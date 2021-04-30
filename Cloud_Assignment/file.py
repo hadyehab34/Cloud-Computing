c0afe4c8dc34fc27b6ef8fdbcf10669f9efc80f7
hadyehab34 <60547183+hadyehab34@users.noreply.github.com>
2021-04-30 16:04:10 +0200

    Add files via upload

---
+++ Cloud_Assignment/file.py
@@ -0,0 +1,22 @@
+import string,re
+
+with open('book1.txt', 'r') as file:
+    book1 = file.read().replace('\n', '')
+
+with open('book2.txt', 'r') as file:
+    book2 = file.read().replace('\n', '')
+
+#Removing punctuation marks 
+book1Final = book1.translate(str.maketrans('', '', string.punctuation+'“”'))
+book2Final = book2.translate(str.maketrans('', '', string.punctuation+'“”'))
+
+#Removing extra spaces between words
+book1Final = re.sub(' +', ' ', book1Final)
+book2Final = re.sub(' +', ' ', book2Final)
+
+book1Words = set(book1Final.split())
+book2Words = set(book2Final.split())
+
+commonWords = book1Words.intersection(book2Words)
+
+print(commonWords)
